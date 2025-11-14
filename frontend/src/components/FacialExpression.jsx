f3722cc6db9cab2f42a21765b62407d8849bfdd7
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-11-15 00:36:09 +0530

    Made function to detect mood on clicking a button

--- frontend/src/components/FacialExpression.jsx
+++ frontend/src/components/FacialExpression.jsx
@@ -3,6 +3,30 @@ import * as faceapi from "face-api.js";
 
 const FacialExpression = () => {
   const videoRef = useRef();
+  const maxVal = (expObject) => {
+    let max = 0;
+    for (let val of Object.values(expObject)) {
+      if (val > max) max = val;
+    }
+    return max;
+  };
+
+  const detectMood = async () => {
+    if (!videoRef.current) return;
+
+    const detections = await faceapi
+      .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
+    if (!detections || !detections.length) {
+      console.log("No face detected");
+    }
+    const expressions = detections[0].expressions;
+    const maxValue = maxVal(expressions);
+    const dominantExpression = Object.keys(expressions).find(
+      (key) => expressions[key] === maxValue
+    );
+    console.log(dominantExpression);
+  };
 
   useEffect(() => {
     const loadModels = async () => {
@@ -10,26 +34,11 @@ const FacialExpression = () => {
       await faceapi.nets.faceExpressionNet.loadFromUri("/models");
     };
 
-    const handleVideoPlay = () => {
-      setInterval(async () => {
-        if (!videoRef.current) return;
-
-        const detections = await faceapi
-          .detectAllFaces(
-            videoRef.current,
-            new faceapi.TinyFaceDetectorOptions()
-          )
-          .withFaceExpressions();
-        console.log(detections[0].expressions);
-      }, 2000);
-    };
-
     const startVideo = () => {
       navigator.mediaDevices
         .getUserMedia({ video: true })
         .then((stream) => {
           videoRef.current.srcObject = stream;
-          videoRef.current.addEventListener("play", handleVideoPlay); // add only once
         })
         .catch((err) => console.error("Error accessing webcam: ", err));
     };
@@ -38,9 +47,22 @@ const FacialExpression = () => {
   }, []);
 
   return (
-    <div className="relative h-screen flex items-center justify-center">
-      <video ref={videoRef} autoPlay muted className="w-full h-[70vh]"></video>
-    </div>
+    <>
+      <div className="relative h-screen flex flex-col items-center justify-center">
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          className="w-full h-[70vh]"
+        ></video>
+        <button
+          className="text-white bg-black px-6 py-4 rounded border-none mt-6 cursor-pointer hover:opacity-80 transition"
+          onClick={detectMood}
+        >
+          Detect Mood
+        </button>
+      </div>
+    </>
   );
 };
 
