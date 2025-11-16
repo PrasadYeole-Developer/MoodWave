b513640e6cb8e19fd959838fe104e5b116312db6
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-11-16 13:53:49 +0530

    Created Schema and Model for song upload

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
+const connectToDB = require("./src/DB/db");
 require("dotenv").config();
+connectToDB();
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
