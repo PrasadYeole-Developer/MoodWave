e3e1452679da9e071c711fe1a2b9dd03eb1c4615
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-11-18 23:52:13 +0530

    Updated initial api of moodwave-backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("MoodWave Backend is running");
+});
 app.use("/songs", songRoutes);
 
 module.exports = app;
